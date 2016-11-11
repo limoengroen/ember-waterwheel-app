41f5c0602fb9289901f8649765a47a87364fc7f1
Chris Hamper <chris.hamper@acquia.com>
2016-11-11 15:39:43 -0500

    Use computed model property to store/display created date as string

--- app/models/node--article.js
+++ app/models/node--article.js
@@ -4,6 +4,10 @@ export default DS.Model.extend({
     nid: DS.attr(),
     uuid: DS.attr(),
     created: DS.attr(),
+    createdDate: Ember.computed('created', function() {
+        let date = new Date(this.get('created') * 1000);
+        return date.toString();
+    }),
     title: DS.attr(),
     body: DS.attr()
 });
