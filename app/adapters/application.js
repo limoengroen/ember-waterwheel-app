2919274baaa3252d89b7a6952d946f92522c1c59
Chris Hamper <chris.hamper@acquia.com>
2016-11-11 14:37:56 -0500

    Append '_format' query parameter in a more robust way

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,12 +3,17 @@ import DS from 'ember-data';
 export default DS.JSONAPIAdapter.extend({
     host: 'http://ember-crud.dd:8080',
     namespace: 'api',
+
     pathForType: function (type) {
         let entityPath;
         switch(type) {
             case 'node--article':
                 entityPath = 'node/article';
         }
-        return entityPath + '?_format=api_json';
+        return entityPath;
+    },
+
+    buildURL: function (modelName, id, snapshot, requestType, query) {
+        return this._super(modelName, id, snapshot, requestType, query) + '?_format=api_json';
     }
 });
