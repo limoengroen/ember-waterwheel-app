7fb19ae33aa723d85ca494d55776343dcc9d1aab
Chris Hamper <chris.hamper@acquia.com>
2016-11-21 09:56:57 -0500

    Add logging of Promise exceptions to console

--- app/app.js
+++ app/app.js
@@ -7,6 +7,12 @@ let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
+// Log exceptions in promises
+Ember.RSVP.configure('onerror', function(e) {
+  console.log(e.message);
+  console.log(e.stack);
+});
+
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
