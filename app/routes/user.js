8a501e5d1ea32b700a414443433f16fbbd45f102
Chris Hamper <chris.hamper@acquia.com>
2016-11-28 15:59:22 -0500

    Get loading of related articles working

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,17 +2,16 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
-        let record = this.store.findRecord('user--user', params.uuid).then((record) => {
-            this.store.query('node--article', {
-                filter: {
-                    uid: {
-                        value: record.get('uid'),
-                    }
+        return this.store.findRecord('user--user', params.uuid);
+    },
+
+    afterModel(user/*, transition*/) {
+        this.store.query('node--article', {
+            filter: {
+                uid: {
+                    value: user.get('uid'),
                 }
-            }).then((articles) => {
-                record.set('articles', articles);
-            })
+            }
         });
-        return record;
     }
 });
