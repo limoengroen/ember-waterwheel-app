7ac434b9c53cfb8fa29bb033dbf78c4cad527db9
Chris Hamper <chris.hamper@acquia.com>
2016-11-28 14:29:45 -0500

    WIP: query for related articles

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,8 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model(params) {
-        let record = this.store.findRecord('user--user', params.uuid);
-//        record.set("articles") = this.store.findAll('node--article', params);
+        let record = this.store.findRecord('user--user', params.uuid).then((record) => {
+            this.store.query('node--article', {
+                filter: {
+                    uid: {
+                        value: record.get('uid'),
+                    }
+                }
+            }).then((articles) => {
+                record.set('articles', articles);
+            })
+        });
         return record;
     }
 });
