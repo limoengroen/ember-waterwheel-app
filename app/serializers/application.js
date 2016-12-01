16bb7a2a5b066530a8bd6a959acffbd41e8739ab
Chris Hamper <chris.hamper@acquia.com>
2016-12-01 14:28:07 -0500

    Customize serializer to handle error messages from Drupal more cleanly

---
+++ app/serializers/application.js
@@ -0,0 +1,25 @@
+import DS from 'ember-data';
+
+export default DS.JSONAPISerializer.extend({
+    extractErrors(store, typeClass, payload, id) {
+        payload = this._super(...arguments);
+
+        let out = {};
+        for (let key in payload) {
+            if (payload.hasOwnProperty(key)) {
+                // Convert newlines to <br/>'s
+                let error = payload[key];
+/*                for (let i = 0; i < error.length; i++) {
+                    error[i] = error[i].replace('\n', '<br/>');
+                }*/
+
+                // Truncate keys to first element before '/'
+                key = key.split('/', 1);
+                out[key[0]] = error;
+            }
+        }
+
+        Ember.Logger.debug(out);
+        return out;
+    }
+});
