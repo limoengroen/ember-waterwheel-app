1f7bf479b3b91d181014935b7b3f9396232ff7c0
Chris Hamper <chris.hamper@acquia.com>
2016-12-06 08:52:18 -0500

    Prevent singularization/pluralization of JSONAPI type keys

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -1,6 +1,12 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
+    modelNameFromPayloadKey(key) {
+        // Only convert underscores to dashes
+        return key.replace(/_/g, '-');
+    },
+
     extractErrors(store, typeClass, payload, id) {
         payload = this._super(...arguments);
 
