205aca6fd58fa590adea298d923697cbf012b881
Chris Hamper <chris.hamper@acquia.com>
2016-12-07 14:54:28 -0500

    Fetch all tags when loading the article route

--- app/routes/article.js
+++ app/routes/article.js
@@ -5,5 +5,18 @@ export default Ember.Route.extend({
         return this.store.findRecord('node--article', params.uuid, {include: 'uid'});
 //        console.log("article record:");
 //        console.log(JSON.parse(JSON.stringify(data)));
+    },
+
+    afterModel() {
+        // Load all tags for autocomplete
+
+    },
+
+    setupController(controller, model) {
+        controller.set('model', model);
+
+        this.store.findAll('taxonomy-term--tag').then(tags => {
+            controller.set('tags', tags)
+        });
     }
 });
