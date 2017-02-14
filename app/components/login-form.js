cc1a71cc4f52d0a3d3ac9d3819549ef0ee921148
Chris Hamper <chris.hamper@acquia.com>
2017-02-14 11:35:21 -0500

    Move login form to component

---
+++ app/components/login-form.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+const {inject: {service}} = Ember;
+
+export default Ember.Component.extend({
+  session: service(),
+
+  actions: {
+    authenticate() {
+      let {username, password} = this.getProperties('username', 'password');
+      this.get('session').authenticate('authenticator:oauth2', username, password).catch((reason) => {
+        if (reason === undefined) {
+          this.set('errorMessage', 'authenticate failed for unknown reasons.');
+        }
+        else {
+          this.set('errorMessage', reason.error || reason);
+        }
+      });
+    }
+  }
+});
--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,21 +1,5 @@
 import Ember from 'ember';
 
-const {inject: {service}} = Ember;
 
 export default Ember.Controller.extend({
-  session: service(),
-
-  actions: {
-    authenticate() {
-      let {username, password} = this.getProperties('username', 'password');
-      this.get('session').authenticate('authenticator:oauth2', username, password).catch((reason) => {
-        if (reason === undefined) {
-          this.set('errorMessage', 'authenticate failed for unknown reasons.');
-        }
-        else {
-          this.set('errorMessage', reason.error || reason);
-        }
-      });
-    }
-  }
 });
