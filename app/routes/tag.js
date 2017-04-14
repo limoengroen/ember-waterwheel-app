baa935c37e3a9fece2dd959ad0166a8b35ec331f
Chris Hamper <chris.hamper@acquia.com>
2017-04-14 13:49:06 -0400

    Side-load articles in tag route

--- app/routes/tag.js
+++ app/routes/tag.js
@@ -7,4 +7,9 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('tag', params.id);
   },
+
+  afterModel(tag /*, transition*/) {
+    // Side-load articles tagged with this tag, so they can be listed by the template
+    this.store.query('article', { 'field_tags.uuid': tag.get('id') });
+  }
 });
